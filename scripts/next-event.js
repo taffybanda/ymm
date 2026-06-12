3830e37caa642352233831b2a8ac0251b0defd5b
Taffy Banda <tafadzwabanda@outlook.com>
2026-06-12 15:56:50 +0200

    glitz font & Tsanzi brand

--- scripts/next-event.js
+++ scripts/next-event.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("June 27, 2026 19:30:00").getTime();
+var countDownDate = new Date("June 27, 2026 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -16,8 +16,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
